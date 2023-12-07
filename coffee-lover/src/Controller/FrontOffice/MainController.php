a18cb1508f2a0e4ab100f259770f0365ac4a5b2f
dauvergne-mickael <mickael.dauvergne@oclock.school>
2023-12-07 17:21:41 +0100

    improve the css and clear the list

--- coffee-lover/src/Controller/FrontOffice/MainController.php
+++ coffee-lover/src/Controller/FrontOffice/MainController.php
@@ -4,8 +4,10 @@ namespace App\Controller\FrontOffice;
 
 
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
@@ -28,5 +30,26 @@ class MainController extends AbstractController
             'homeArticle' => $homeArticle,
             'randomArticle' => $randomArticle
         ]);
+    }
+
+
+    /**
+     * @Route("/search", name="app_search", methods={"GET"})
+     */
+    public function search(Request $request, ArticleRepository $articleRepository): JsonResponse
+    {
+        // On recupere le formulaire via 'searchTerm' 
+        $query = $request->query->get('searchTerm');
+        // On recup la méthode du repository qui recherche par titre
+        $results = $articleRepository->searchArticle($query);
+        // on converti les resultatst dans un tableau asso
+        $data = [];
+        foreach ($results as $result) {
+            $data[] = [
+                'title' => $result->getTitle(),
+            ];
+        }
+        // On renvois la réponse en JSON
+        return new JsonResponse($data);
     }
 }
