f2848bce7f86d1d122d3c76a5dce5110fc4c48bf
dauvergne-mickael <mickael.dauvergne@oclock.school>
2023-12-08 14:39:59 +0100

    mainController

--- src/Controller/FrontOffice/ArticleController.php
+++ src/Controller/FrontOffice/ArticleController.php
@@ -3,45 +3,32 @@
 namespace App\Controller\FrontOffice;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/articles")
- * 
  */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/{tag}", name="app_categorie")
+     * @Route("/{categorySlug}", name="app_categorie")
      */
-
-    public function list(ArticleRepository $articleRepository, Article $article): Response
+    public function getArticlesByCategory(string $categorySlug, CategoryRepository $categoryRepository, ArticleRepository $articleRepository)
     {
-        // Display all articles related to a tag from the database.
+        $category = $categoryRepository->findOneBy([
+            'slug' => $categorySlug,
+        ]);
 
-        $articles = $articleRepository->findAllByTag(['tag' => $article->getTag()]);
-
+        $articles = $articleRepository->findBy([
+            'category' => $category,
+        ]);
         return $this->render('article/list.html.twig', [
             'articles' => $articles,
-        ]);
-    }
-
-    /**
-     * @Route("/{tag}/{title}", name="app_article_show")
-     * 
-     */
-    public function show(Article $article)
-    {
-
-        // Error page if the article does not exist."
-        if ($article === null) {
-            return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
-        }
-        return $this->render('article/show.html.twig', [
-            'article' => $article,
         ]);
     }
 }
