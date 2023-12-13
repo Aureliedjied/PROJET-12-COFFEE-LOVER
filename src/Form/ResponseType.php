ea355b23ca208e5c0eb5bc6400c71de82f8eebf2
dauvergne-mickael <mickael.dauvergne@oclock.school>
2023-12-13 20:46:51 +0100

    add scroll-point in back-office + delete duplicate div iarticles/show

--- src/Form/ResponseType.php
+++ src/Form/ResponseType.php
@@ -6,6 +6,7 @@ use App\Entity\Response;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ResponseType extends AbstractType
@@ -13,7 +14,7 @@ class ResponseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text_response', TextareaType::class, [
+            ->add('text_response', TextType::class, [
                 'label' => 'Réponse'
             ])
             ->add('is_correct')
